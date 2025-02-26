f768208fc55718562ce2b9cbfa48cea5b9e20a86
TimFengDev <timf3801@gmail.com>
2025-02-27 00:38:07 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const helloWorld = "Hello World";
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text(
+          helloWorld,
+        ),
+      ),
+    ),
+  );
+}
